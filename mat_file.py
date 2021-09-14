2510264bc94a3a60d4b022b9b99da236441ff704
Vyankatesh Goski <vyankateshgoski09@gmail.com>
2021-09-14 13:20:19 +0530

    change done

--- mat_file.py
+++ mat_file.py
@@ -8,7 +8,7 @@ from scipy.io.wavfile import write
 mat1 = loadmat('45.mat')
 h1 = mat1['impulse_response'][:,0]
 fs1, s1 = wavfile.read('1_0a139a.wav')
-convolved_speech_data1 = np.convolve(s1[:80000],h1[:80000])
+convolved_speech_data1 = np.convolve(s1[:80000],h1[:5000])
 print(convolved_speech_data1,size(s1),size(h1))
 write('sound1.wav',fs1,convolved_speech_data1.astype(np.int16))
 plt.subplot(3,1,1)
@@ -23,7 +23,7 @@ plt.show()
 mat2 = loadmat('135.mat')
 fs2, s2 = wavfile.read('1_00bdad.wav')
 h2 = mat2['impulse_response'][:,0]
-convolved_speech_data2 = np.convolve(s2[:80000],h2[:80000])
+convolved_speech_data2 = np.convolve(s2[:80000],h2[:5000])
 print(convolved_speech_data2,size(s2),size(h2))
 write('sound2.wav',fs2,convolved_speech_data2.astype(np.int16))
 plt.subplot(3,1,1)
